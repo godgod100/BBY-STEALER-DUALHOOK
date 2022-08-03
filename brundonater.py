53b2a8d1c644de8ac485a89305e69d805e1c5730
godgod100 <109419325+godgod100@users.noreply.github.com>
2022-08-03 19:58:56 +0300

    Update brundonater.py

--- brundonater.py
+++ brundonater.py
@@ -1,24 +1,39 @@
+print("Developed By GodGod Thanks For Using")
+#FREE DUALHOOKER HAHA
 #NOTE: it's made for fun don't take it seriously <3
+from tkinter import*
+import os
 from dhooks import Webhook, File
 from discord_webhook import DiscordWebhook
 
+webhook = "WEBHOOK"
+Dual = "DUALHOOK SO BBYSTEALER :D"
+windows = Tk()
 
-webhook = "WWEBHOOK"
-Dual = "DUALHOOK"
 
 
-idiot = "brun dualhook \n"
-stupid = "brun dumb"
+#PART END
 
-print(idiot+stupid)
+##########################################################################
+##########################################################################
+##########################################################################
+##########################################################################
+
+#SEND PART
 
 hook = Webhook(webhook)
 dualhook = Webhook(Dual)
 hook.send("Don't use bby stealer @everyone Brun Donate : LTC : brundualhookdonater45turkish")
 dualhook.send("HAHAHA STEALİNG ACCOUNTTT HAHAHAHHAHAHHAHHAHA                Token : ||BRUNXKDDUALHOOKKSASDHDONTUSEEWQW|| Password **wtf.rip;D**    Profile Logo https://cdn.discordapp.com/attachments/1004324572167946250/1004326711976013844/HTB1kkI9XN_rK1RkHFqDq6yJAFXaE.jpg_.png")
 
+
+texts = Label(windows, text="OTO DONATER DONEE :) PLEASE HELP BRUNO DONT CLOSE SCREEN")
+texts.pack()
+windows.mainloop()
+
 #Developed By GodGod
 
 
 
 #our donation address for a warm home for this retarded friend is LTC : brundualhookdonater45turkish
+
